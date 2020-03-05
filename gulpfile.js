57bd522303a4057bf69f6c1d21bbc563d437e362
Andrey Kolybelnikov <a.kolybelnikov@gmail.com>
2020-03-05 12:11:20 +0100

    Gulp scripts modified.

--- gulpfile.js
+++ gulpfile.js
@@ -2,48 +2,42 @@ const gulp = require("gulp");
 const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
 const browserSync = require("browser-sync").create();
-const cssnano = require('gulp-cssnano');
-const uncss = require('gulp-uncss');
+const cssnano = require("gulp-cssnano");
+const uncss = require("gulp-uncss");
 
 gulp.task("sass", function() {
   return gulp
     .src("assets/scss/*.scss")
     .pipe(sass())
     .pipe(autoprefixer())
-    .pipe(gulp.dest("assets/css"));
-});
-
-
-gulp.task("reload", function(done) {
-  browserSync.reload();
-  done();
-});
-
-gulp.task("watch", function() {
-  gulp.watch("assets/scss/**/*.scss", gulp.series("sass", "reload"));
-});
-
-gulp.task("watch-html", function() {
-  gulp.watch("*.html", gulp.series("reload"));
+    .pipe(gulp.dest("assets/css"))
+    .pipe(browserSync.stream());
 });
 
 gulp.task("serve", function() {
   browserSync.init({
-    server: ".",
+    server: {
+      baseDir: "./"
+    },
     port: 4000
   });
+
+  gulp.watch("assets/scss/*scss", gulp.series('sass'));
+  gulp.watch("index.html").on("change", browserSync.reload);
 });
 
-gulp.task("live-server", gulp.series("serve", "watch"));
+gulp.task("live-server", gulp.series("serve"));
 
 gulp.task("default", function() {
   return gulp
     .src("assets/scss/*.scss")
     .pipe(sass())
     .pipe(autoprefixer())
-    .pipe(uncss({
-      html: ['index.html']
-    }))
+    .pipe(
+      uncss({
+        html: ["index.html"]
+      })
+    )
     .pipe(cssnano())
     .pipe(gulp.dest("assets/css"));
-})
+});
